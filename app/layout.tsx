d7566024674b3ee7312a397fe3c0c166b0a0292c
alvin-al <alvvxal@gmail.com>
2024-10-17 07:59:07 +0700

    Add file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import '@/app/ui/global.css'
+import "@/app/ui/global.css";
+import { Inter, Lusitana } from "next/font/google";
 
 export default function RootLayout({
   children,
@@ -6,8 +7,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={"${Inter.className} antialiased"}>{children}</body>
     </html>
   );
 }
